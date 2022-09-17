0c51c31f00ce112b4e70c6b6cbce48bb008e4e2f
ajurna <ajurna@gmail.com>
2022-09-17 17:51:31 +0000

    cleaned up server2.py

--- server2.py
+++ server2.py
@@ -41,4 +41,8 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info('Quitting')
+        quit()
