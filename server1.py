86a5af3d286e43283a3548c7772e3f6b5fc8a386
ajurna <ajurna@gmail.com>
2022-09-17 17:29:56 +0000

    cleaned up server1.py

--- server1.py
+++ server1.py
@@ -8,23 +8,32 @@ HOST = "0.0.0.0"
 PORT = 40000
 
 
+async def write_message(writer: asyncio.StreamWriter, message: dict):
+    writer.write(json.dumps(message).encode(encoding='utf8'))
+    writer.write(b'\n')
+    await writer.drain()
+
+
 async def handle(r: asyncio.StreamReader, w: asyncio.StreamWriter):
-    while not r.at_eof():
-        data = await r.readline()
-        try:
-            resp = parse_response(data)+b'\n'
-            logger.debug(f"{data} -> {resp}")
-            w.write(resp)
-            await w.drain()
-        except (json.decoder.JSONDecodeError, UnicodeDecodeError, ValueError, KeyError):
-            logger.debug(f"{data}")
-            w.write(b'{"error":"invalid_json"}\n')
-            await w.drain()
-    w.close()
+    try:
+        while not r.at_eof():
+            data = await r.readline()
+            try:
+                resp = parse_response(data)
+                await write_message(w, resp)
+            except (json.decoder.JSONDecodeError, UnicodeDecodeError):
+                await write_message(w, {"error": "invalid json"})
+            except ValueError:
+                await write_message(w, {"error": "invalid value in number or method"})
+            except KeyError:
+                await write_message(w, {"error": "missing method or number"})
+        w.close()
+    except ConnectionResetError:
+        logger.error('Connection was reset')
 
 
-def parse_response(data: bytes):
-    false_resp = b'{"method":"isPrime","prime":false}'
+def parse_response(data: bytes) -> dict:
+    false_resp = {"method": "isPrime", "prime": False}
     parsed_data: Dict = json.loads(data)
     if parsed_data['method'] != 'isPrime':
         raise ValueError
@@ -35,14 +44,14 @@ def parse_response(data: bytes):
         raise ValueError
     if number < 2:
         return false_resp
-    return json.dumps({
+    return {
         'method': 'isPrime',
         "prime": is_prime(number)
-    }).encode()
-    
+    }
+
 
 def is_prime(n):
-    if n % 2 == 0 and n > 2: 
+    if n % 2 == 0 and n > 2:
         return False
     return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
 
