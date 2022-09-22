3debbf6fb4a20362d782019d9c0b0b413a9dfc41
ajurna <ajurna@gmail.com>
2022-09-22 22:59:58 +0000

    solution for problem 3

---
+++ server3.py
@@ -0,0 +1,79 @@
+import asyncio
+from typing import NamedTuple
+
+from loguru import logger
+
+HOST = "0.0.0.0"
+PORT = 40000
+
+
+class ReaderWriter(NamedTuple):
+    reader: asyncio.StreamReader
+    writer: asyncio.StreamWriter
+
+
+class ChatServer:
+
+    def __init__(self):
+        self.connections = {}
+        self.server = None
+
+    async def start(self):
+        self.server = await asyncio.start_server(self.accept_connection, "0.0.0.0", 40000)
+        async with self.server:
+            logger.info("Server Ready.")
+            await self.server.serve_forever()
+
+    def broadcast(self, message, user=None):
+        logger.info(message)
+        for username, rw in self.connections.items():
+            if username != user:
+                rw.writer.write((message + "\n").encode("utf-8"))
+
+    async def prompt_username(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        while True:
+            writer.write("Welcome to budgetchat! What shall I call you?\n".encode("utf-8"))
+            await writer.drain()
+            data = (await reader.readline()).decode("utf-8")
+            username = data.strip()
+            if not username.isalnum():
+                writer.write("Sorry, that username is invalid.\n".encode("utf-8"))
+                return None
+            if username and username not in self.connections:
+                writer.write(f'* The room contains: {", ".join(self.connections)}\n'.encode('utf-8'))
+                await writer.drain()
+                self.connections[username] = ReaderWriter(reader, writer)
+                return username
+            writer.write("Sorry, that username is taken.\n".encode("utf-8"))
+            return None
+
+    async def handle_connection(self, username, reader: asyncio.StreamReader):
+        while not reader.at_eof():
+            data = (await reader.readline()).decode("utf-8").strip()
+            if len(data) == 0:
+                continue
+            self.broadcast(f"[{username}] {data}", user=username)
+        del self.connections[username]
+
+    async def accept_connection(self, reader, writer):
+        username = await self.prompt_username(reader, writer)
+        if username is not None:
+            self.broadcast(f"* {username} has entered the room", user=username)
+            await self.handle_connection(username, reader)
+            self.broadcast(f"* {username} has left the room")
+        await writer.drain()
+        writer.close()
+
+
+async def main():
+    server = ChatServer()
+    await server.start()
+
+
+if __name__ == "__main__":
+    try:
+        lock = asyncio.Lock()
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info('Quitting')
+        quit()
