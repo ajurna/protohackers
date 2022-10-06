a0675d12f795b3e7b717f87ac019f058a693c49c
ajurna <ajurna@gmail.com>
2022-10-06 17:41:07 +0000

    initial solution for problem 4

---
+++ server4.py
@@ -0,0 +1,56 @@
+import asyncio
+from typing import Dict, NamedTuple
+
+from loguru import logger
+
+HOST = "0.0.0.0"
+PORT = 40000
+
+
+class Message(NamedTuple):
+    req: str
+    int_1: int
+    int_2: int
+
+
+class DBServer:
+    
+    def __init__(self):
+        self.database = {}
+    
+    def connection_made(self, transport):
+        self.transport = transport
+        
+
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        logger.info(f'Received : {message}')
+        if '=' in message:
+            key, val = message.split('=', 1)
+            self.database[key] = val
+        elif message == 'version':
+            self.transport.sendto(b"version=Ken's Key-Value Store 1.0", addr)
+        else:
+            value = self.database[message]
+            data = f'{message}={value}'
+            self.transport.sendto(data.encode(), addr)
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    logger.info("Server Ready.")
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: DBServer(),
+        local_addr=(HOST, PORT))
+    try:
+        await asyncio.sleep(3600)  # Serve for 1 hour.
+    finally:
+        transport.close()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info('Quitting')
+        quit()
