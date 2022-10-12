9a0882645e6b47ecd82f63762eb196c275394ed4
Peter Dwyer <peter.dwyer@clanwilliamhealth.com>
2022-10-12 17:03:24 +0100

    added rust smoke test

--- rust/server00/src/main.rs
+++ rust/server00/src/main.rs
@@ -1,19 +1,19 @@
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("0.0.0.0:40000").await?;
-
+    println!("Ready to accept connections");
     loop {
         let (mut socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
+            let mut buf: Vec<u8> = Vec::new();
             // In a loop, read data from the socket and write the data back.
             loop {
-                let n = match socket.read(&mut buf).await {
+                let _n = match socket.read_to_end(&mut buf).await {
                     // socket closed
                     Ok(n) if n == 0 => return,
                     Ok(n) => n,
@@ -23,8 +23,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     }
                 };
 
+                let message = String::from_utf8_lossy(&buf);
+                println!("Message: {:?}", message);
+
                 // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                if let Err(e) = socket.write_all(&buf).await {
                     eprintln!("failed to write to socket; err = {:?}", e);
                     return;
                 }
