27ab5e191baba2d3f27b3bfeecedcbcaa9862846
Peter Dwyer <peter.dwyer@clanwilliamhealth.com>
2022-11-03 11:08:53 +0000

    before days are accounted for

--- python/server6.py
+++ python/server6.py
@@ -76,20 +76,23 @@ class SpeedDaemon(TcpServer):
     async def dispatcher(self):
         while True:
             ticket = await self.ticket_queue.get()
+            # logger.info(f'Processing ticket: {ticket=}')
             dispatchers_to_remove = []
-            for writer in self.dispatchers[ticket.road]:
-                writer: asyncio.StreamWriter
-                socket = writer.get_extra_info(name='socket')
-                if not socket._sock._closed:
-                    writer.write(ticket.data())
-                    break
-                else:
-                    dispatchers_to_remove.append(writer)
-            for writer in dispatchers_to_remove:
-                self.dispatchers[ticket.road].remove(writer)
+            ticket_sent = False
+            for conn in self.dispatchers[ticket.road]:
+                conn: Conn
+                conn.writer.write(ticket.data())
+                logger.info(f'ticket sent: {ticket=}')
+                ticket_sent = True
+            if not ticket_sent:
+                await self.ticket_queue.put(ticket)
+                # logger.info(f'requeue ticket: {ticket=}')
+                await asyncio.sleep(1)
+            for conn in dispatchers_to_remove:
+                self.dispatchers[ticket.road].remove(conn)
 
     async def Plate(self, conn):
-        plate_length = await self.read_u8(conn.reader)
+        plate_length = await self.read_u8(conn)
         plate = await self.read_str(conn, plate_length)
         timestamp = await self.read_u32(conn)
         return Plate(plate, timestamp)
@@ -109,8 +112,17 @@ class SpeedDaemon(TcpServer):
             new_roads.append(road)
         return new_roads
 
-    def add_sighting(self, plate):
-        pass
+    async def add_sighting(self, plate: Plate, camera: Camera):
+        logger.info(f"Sighting: {camera=} {plate=}")
+        if plate.plate not in self.roads[camera.road]:
+            self.roads[camera.road][plate.plate] = []
+        self.roads[camera.road][plate.plate].append(Sighting(camera.mile, plate.timestamp))
+        if plate.plate not in self.tickets and len(self.roads[camera.road][plate.plate]) > 1:
+            sightings = await self.limit_broken(self.roads[camera.road][plate.plate], camera.limit)
+            if sightings:
+                logger.info(f"Sighting: {camera=} {plate.plate=} {sightings=}")
+                self.tickets.add(plate.plate)
+                await self.send_ticket(plate, camera, sightings)
 
     async def accept_connection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         conn = Conn(reader, writer)
@@ -125,81 +137,41 @@ class SpeedDaemon(TcpServer):
                     case 0x20:
                         plate = await self.Plate(conn)
                         if camera:
-                            self.add_sighting(plate)  # TODO:
+                            logger.info(f'Plate: {plate=}')
+                            await self.add_sighting(plate, camera)
                     case 0x40:  # WantHeartbeat
                         heartbeat_time = await self.WantHeartbeat(conn)
                         if not heartbeat and heartbeat_time:
-                            heartbeat = asyncio.create_task(self.heartbeat(reader, writer, heartbeat_time / 10))
+                            heartbeat = asyncio.create_task(self.heartbeat(conn, heartbeat_time / 10))
                     case 0x80:
                         new_camera = await self.IAmCamera(conn)
                         if not camera:
                             camera = new_camera
+                            logger.info(f'Camera: {camera=}')
                             if camera.road not in self.roads:
                                 self.roads[camera.road] = {}
                     case 0x81:
                         roads = await self.IAmDispatcher(conn)
                         if not dispatcher and not camera:
+                            dispatcher = roads
                             for road in roads:
                                 self.dispatchers[road].append(conn)
-            if msg_type == 0x20:
-                plate_length = await self.read_u8(reader)
-                plate = "".join([chr(await self.read_u8(reader)) for _ in range(plate_length)])
-                timestamp = await self.read_u32(reader)
-                # logger.info(f"Plate: {plate=} {mile=} {timestamp=}")
-                if plate not in self.roads[road]:
-                    self.roads[road][plate] = []
-                self.roads[road][plate].append(Sighting(mile, timestamp))
-                if plate not in self.tickets and len(self.roads[road][plate]) > 1:
-                    if sightings := await self.limit_broken(self.roads[road][plate], limit, plate):
-                        self.tickets.add(plate)
-                        await self.send_ticket(plate, road, sightings)
+
         except IncompleteReadError:
-            logger.error('Incomplete read error')
+            logger.warning('Client Disconnected')
+            if heartbeat:
+                heartbeat.cancel()
             writer.close()
 
-    async def camera(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        road, mile, limit = struct.unpack('>HHH', await reader.readexactly(6))
-        if road not in self.roads:
-            self.roads[road] = {}
-        # logger.info(f'Camera {road=} {mile=} {limit=}')
-        while not reader.at_eof():
-            msg_type = await self.read_u8(reader)
-
-
-
-            if msg_type == 0x20:
-                plate_length = await self.read_u8(reader)
-                plate = "".join([chr(await self.read_u8(reader)) for _ in range(plate_length)])
-                timestamp = await self.read_u32(reader)
-                # logger.info(f"Plate: {plate=} {mile=} {timestamp=}")
-                if plate not in self.roads[road]:
-                    self.roads[road][plate] = []
-                self.roads[road][plate].append(Sighting(mile, timestamp))
-                if plate not in self.tickets and len(self.roads[road][plate]) > 1:
-                    if sightings := await self.limit_broken(self.roads[road][plate], limit, plate):
-                        self.tickets.add(plate)
-                        await self.send_ticket(plate, road, sightings)
-            elif msg_type == 0x40:  # WantHeartbeat
-                heartbeat_time = await self.read_u32(reader)
-                logger.info(f'{heartbeat_time=}')
-                if heartbeat_time > 0:
-                    asyncio.create_task(self.heartbeat(reader, writer, heartbeat_time / 10))
-            elif msg_type == 0x80:
-                await reader.readexactly(6)
-                await self.send_error(writer, 'Already Camera')
-            elif msg_type == 0x81:
-                for _ in range(await self.read_u8(reader)):
-                    await self.read_u16(reader)
-                await self.send_error(writer, 'Already Camera')
-
-    async def send_ticket(self, plate, road, sightings: Tuple[Sighting, Sighting, int]):
+    async def send_ticket(self, plate: Plate, camera: Camera, sightings: Tuple[Sighting, Sighting, int]):
         sighting1, sighting2, speed = sightings
-        ticket = Ticket(plate, road, sighting1.mile, sighting1.timestamp, sighting2.mile, sighting2.timestamp, speed)
+        ticket = Ticket(plate.plate, camera.road, sighting1.mile, sighting1.timestamp, sighting2.mile,
+                        sighting2.timestamp, speed)
         await self.ticket_queue.put(ticket)
         logger.info(f'Sending Ticket: {ticket=}')
 
-    async def limit_broken(self, sightings: List[Sighting], limit: int, plate) -> Optional[
-        Tuple[Sighting, Sighting, int]]:
+    async def limit_broken(self, sightings: List[Sighting], limit: int) -> \
+            Optional[Tuple[Sighting, Sighting, int]]:
         sightings.sort(key=lambda x: x[1])
         for sighting1, sighting2 in zip(sightings, sightings[1:]):
             time = abs(sighting2.timestamp - sighting1.timestamp)
@@ -210,15 +182,17 @@ class SpeedDaemon(TcpServer):
                 # logger.info(f'Speed high: {sighting1=} {sighting2=}')
                 return sighting1, sighting2, int(round(speed, 0) * 100)
 
-    async def heartbeat(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter, heartbeat: float):
+    async def heartbeat(self, conn: Conn, heartbeat: float):
         logger.info(f'Beating: {heartbeat}')
         try:
-            while not reader.at_eof():
+            while not conn.reader.at_eof():
                 await asyncio.sleep(heartbeat)
                 # logger.info('Beat')
-                writer.write(b"\x41")
-                await writer.drain()
+                conn.writer.write(b"\x41")
+                await conn.writer.drain()
         except ConnectionResetError:
+            return
+        except asyncio.CancelledError:
             return
 
 
