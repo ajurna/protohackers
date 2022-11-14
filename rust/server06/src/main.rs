02d4b3a01980e6da15c07f9a285439e2f8c762b9
Peter Dwyer <peter.dwyer@clanwilliamhealth.com>
2022-11-14 12:14:59 +0000

    finished rust server 6

--- rust/server06/src/main.rs
+++ rust/server06/src/main.rs
@@ -1,27 +1,32 @@
 extern crate core;
 
 use std::collections::{HashMap, HashSet};
-use std::sync::{Arc};
-use std::thread::sleep;
+use std::io::ErrorKind::WouldBlock;
+use std::sync::Arc;
 use std::time::Duration;
+
 use queues::{IsQueue, Queue};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
-use tokio::net::TcpListener;
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::net::TcpListener;
 use tokio::sync::Mutex;
+use tokio::time::sleep;
 
 type ConnectionHandler = Arc<Mutex<BufWriter<OwnedWriteHalf>>>;
+
 #[derive(Clone, Debug)]
 struct Sighting {
     mile: u16,
     timestamp: u32,
-    day: u32
+    day: u32,
 }
+
 #[derive(Debug)]
 struct Plate {
     plate: String,
-    timestamp: u32
+    timestamp: u32,
 }
+
 type Road = u16;
 
 type RoadData = HashMap<String, Vec<Sighting>>;
@@ -36,13 +41,12 @@ struct Ticket {
     timestamp1: u32,
     mile2: u16,
     timestamp2: u32,
-    speed: u16
+    speed: u16,
 }
 
 impl Ticket {
     fn to_bytes(&self) -> Vec<u8> {
-
-        let mut out:Vec<u8> = vec![];
+        let mut out: Vec<u8> = vec![];
         out.push(0x21 as u8);
         out.push(self.plate.len() as u8);
         out.extend(self.plate.as_bytes());
@@ -52,9 +56,10 @@ impl Ticket {
         out.extend(self.mile2.to_be_bytes());
         out.extend(self.timestamp2.to_be_bytes());
         out.extend(self.speed.to_be_bytes());
-        return out
+        return out;
     }
 }
+
 type TicketQueue = Arc<Mutex<Queue<Ticket>>>;
 type TicketSent = Arc<Mutex<HashSet<(u32, String)>>>;
 
@@ -62,19 +67,19 @@ type TicketSent = Arc<Mutex<HashSet<(u32, String)>>>;
 struct Camera {
     road: Road,
     mile: u16,
-    limit: u16
+    limit: u16,
 }
+
 struct LimitBroke {
     sighting1: Sighting,
     sighting2: Sighting,
-    speed: u16
+    speed: u16,
 }
-
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("0.0.0.0:40000").await?;
+    let listener = TcpListener::bind("0.0.0.0:54321").await?;
     let data: SightingData = Arc::new(Mutex::new(HashMap::new()));
     let dispatchers: Dispatchers = Arc::new(Mutex::new(HashMap::new()));
     let ticket_queue: TicketQueue = Arc::new(Mutex::new(Queue::new()));
@@ -98,34 +103,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let writer = BufWriter::new(writer);
             let writer: ConnectionHandler = Arc::new(Mutex::new(writer));
 
-            // In a loop, read data from the socket and write the data back.
             loop {
-                let msg_type = match reader.read_u8().await {
-                    Ok(v) => v,
+                let mut buf = [0 as u8];
+                let _n = match reader.try_read(&mut buf) {
+                    Ok(n) if n == 0 => {
+                        sleep(Duration::from_millis(500)).await;
+                        continue;
+                    }
+                    Ok(n) => n,
+                    Err(e)  if e.kind() == WouldBlock => {
+                        sleep(Duration::from_millis(500)).await;
+                        continue;
+                    }
                     Err(e) => {
                         eprintln!("Error: {:?}", e);
-                        return
+                        return;
                     }
                 };
-                // println!("{:?}", msg_type);
+                let msg_type = buf[0];
                 match msg_type {
                     0x10 => { // Error (Server->Client)
                         write_error(&writer, "Error not accepted".to_owned()).await
-                    },
+                    }
                     0x20 => { // Plate (Client->Server)
                         let plate = get_plate(&mut reader).await;
-                        println!("Plate: {:?}", plate);
+                        // println!("Plate: {:?}", plate);
                         match &camera {
                             Some(camera) => {
                                 add_sighting(camera, plate, &data, &ticket_queue, &ticket_sent).await;
                                 ()
-                            },
+                            }
                             None => write_error(&writer, "Plates only from camera's".to_owned()).await,
                         }
-                    },
+                    }
                     0x21 => { // Ticket (Server->Client)
-
-                    },
+                        get_ticket(&mut reader).await;
+                        write_error(&writer, "Tickets not accepted".to_owned()).await
+                    }
                     0x40 => { // WantHeartbeat (Client->Server)
                         if !heart_beating {
                             setup_heartbeat(&writer, reader.read_u32().await.unwrap()).await;
@@ -133,49 +147,62 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         } else {
                             write_error(&writer, "Heartbeat Already Active".to_owned()).await
                         }
-                        println!("Heartbeat setup")
-                    },
+                    }
                     0x41 => { // Heartbeat (Server->Client)
                         write_error(&writer, "Heartbeat not accepted".to_owned()).await
-                    },
+                    }
                     0x80 => { // IAmCamera (Client->Server)
                         let new_camera = get_camera(&mut reader).await;
-                        match &camera {
-                            Some(_c) => {
-                                write_error(&writer, "Camera Assigned".to_owned()).await
-                            },
-                            None => {
-                                camera=Some(new_camera);
-                                println!("Camera: {:?}", camera);
+                        if !is_dispatcher {
+                            match &camera {
+                                Some(_c) => {
+                                    write_error(&writer, "Camera Assigned".to_owned()).await;
+                                }
+                                None => {
+                                    camera = Some(new_camera);
+                                    println!("Camera: {:?}", camera);
+                                }
                             }
+                        } else {
+                            write_error(&writer, "Already Dispatcher".to_owned()).await;
                         }
-                    },
+                    }
                     0x81 => { // IAmDispatcher (Client->Server)
                         let dispatcher_roads = get_dispatcher(&mut reader).await;
-                        if !is_dispatcher {
-                            is_dispatcher = true;
-                            add_dispatchers(&dispatchers, dispatcher_roads, &writer).await;
+                        match &camera {
+                            Some(_c) => {
+                                write_error(&writer, "Already Camera".to_owned()).await;
+                            }
+                            None => {
+                                if !is_dispatcher {
+                                    is_dispatcher = true;
+                                    add_dispatchers(&dispatchers, dispatcher_roads, &writer).await;
+                                } else {
+                                    write_error(&writer, "Dispatcher already set".to_owned()).await;
+                                }
+                            }
                         }
-                    },
+                    }
                     _ => { // Invalid Message
                         write_error(&writer, "Heartbeat not accepted".to_owned()).await;
                     }
-
                 }
             }
         });
     }
 }
 
-async fn setup_heartbeat(writer: &ConnectionHandler, beat_length: u32){
+async fn setup_heartbeat(writer: &ConnectionHandler, beat_length: u32) {
+    println!("Heartbeat length {:?}", beat_length);
     if beat_length == 0 {
         return;
     }
     let writer = writer.clone();
-    let beat_length = Duration::from_millis((beat_length*100) as u64);
+
     tokio::spawn(async move {
+        let beat_length = Duration::from_millis((beat_length * 100) as u64);
         loop {
-            sleep(beat_length);
+            sleep(beat_length).await;
             {
                 let mut writer = writer.lock().await;
                 writer.write_u8(0x41).await.unwrap();
@@ -188,10 +215,22 @@ async fn setup_heartbeat(writer: &ConnectionHandler, beat_length: u32){
     });
 }
 
+async fn get_ticket(reader: &mut OwnedReadHalf) {
+    let length = reader.read_u8().await.unwrap();
+    let mut buf: Vec<u8> = vec![0; length as usize];
+    reader.read_exact(&mut buf).await.unwrap();
+    let _road = reader.read_u16().await.unwrap();
+    let _mile1 = reader.read_u16().await.unwrap();
+    let _timestamp1 = reader.read_u32().await.unwrap();
+    let _mile2 = reader.read_u16().await.unwrap();
+    let _timestamp2 = reader.read_u32().await.unwrap();
+    let _speed = reader.read_u16().await.unwrap();
+}
+
 async fn get_plate(reader: &mut OwnedReadHalf) -> Plate {
     let plate_length = reader.read_u8().await.unwrap();
 
-    let mut buf:Vec<u8> = vec![0; plate_length as usize];
+    let mut buf: Vec<u8> = vec![0; plate_length as usize];
     reader.read_exact(&mut buf).await.unwrap();
     let plate = String::from_utf8(buf).unwrap();
 
@@ -199,9 +238,7 @@ async fn get_plate(reader: &mut OwnedReadHalf) -> Plate {
 }
 
 async fn add_sighting(camera: &Camera, plate: Plate, data: &SightingData, ticket_queue: &TicketQueue, ticket_sent: &TicketSent) {
-    println!("sighting: requesting lock");
     let mut data = data.lock().await;
-    println!("sighting: lock acquired");
     if !data.contains_key(&camera.road) {
         data.insert(camera.road.clone(), HashMap::new());
     }
@@ -209,22 +246,21 @@ async fn add_sighting(camera: &Camera, plate: Plate, data: &SightingData, ticket
         data.get_mut(&camera.road).unwrap().insert(plate.plate.to_string(), vec![]);
     }
 
-    let sighting = Sighting{ mile: camera.mile, timestamp: plate.timestamp, day: plate.timestamp/86400 };
+    let sighting = Sighting { mile: camera.mile, timestamp: plate.timestamp, day: plate.timestamp / 86400 };
+    println!("Sighting: {:?}, {:?}, {:?}", camera.road, plate.plate, sighting);
     data.get_mut(&camera.road).unwrap().get_mut(&plate.plate).unwrap().push(sighting);
     if data.get(&camera.road).unwrap().get(&plate.plate).unwrap().len() > 1 {
         for limit_broken in get_limit_broken(camera, data.get_mut(&camera.road).unwrap().get_mut(&plate.plate).unwrap()).await {
-            let ticket_tuple = (limit_broken.sighting1.day, plate.plate.to_owned());
-            println!("sighting: sent lock requested");
+            let ticket_tuple_1 = (limit_broken.sighting1.day, plate.plate.to_owned());
+            let ticket_tuple_2 = (limit_broken.sighting2.day, plate.plate.to_owned());
             let mut ticket_sent = ticket_sent.lock().await;
-            println!("sighting: sent lock acquired");
-            if !ticket_sent.contains(&ticket_tuple) {
+            if !ticket_sent.contains(&ticket_tuple_1) && !ticket_sent.contains(&ticket_tuple_2) {
                 send_ticket(ticket_queue, &plate, &camera, limit_broken).await;
-                ticket_sent.insert(ticket_tuple);
+                ticket_sent.insert(ticket_tuple_1);
+                ticket_sent.insert(ticket_tuple_2);
             }
-            println!("sighting: sent lock released");
         }
     }
-    println!("sighting: lock released");
 }
 
 async fn get_limit_broken(camera: &Camera, sightings: &mut Vec<Sighting>) -> Vec<LimitBroke> {
@@ -237,22 +273,22 @@ async fn get_limit_broken(camera: &Camera, sightings: &mut Vec<Sighting>) -> Vec
         let distance = (s1.mile as i64 - s2.mile as i64).abs() as f64;
         let speed = ((distance / time) * 60.0 * 60.0).round() as u64;
         if speed > camera.limit as u64 {
-            broken.push(LimitBroke{sighting1: s1.clone(), sighting2: s2.clone(), speed: (speed*100) as u16})
+            broken.push(LimitBroke { sighting1: s1.clone(), sighting2: s2.clone(), speed: (speed * 100) as u16 })
         }
     }
     broken
 }
 
-async fn get_camera(reader: &mut OwnedReadHalf) -> Camera{
+async fn get_camera(reader: &mut OwnedReadHalf) -> Camera {
     let road = reader.read_u16().await.unwrap();
     let mile = reader.read_u16().await.unwrap();
     let limit = reader.read_u16().await.unwrap();
-    Camera{road, mile, limit}
+    Camera { road, mile, limit }
 }
 
-async fn get_dispatcher(reader: &mut OwnedReadHalf) -> Vec<u16>{
+async fn get_dispatcher(reader: &mut OwnedReadHalf) -> Vec<u16> {
     let road_count = reader.read_u8().await.unwrap();
-    let mut buf:Vec<u16> = vec![];
+    let mut buf: Vec<u16> = vec![];
     for _ in 0..road_count {
         buf.push(reader.read_u16().await.unwrap())
     }
@@ -260,7 +296,7 @@ async fn get_dispatcher(reader: &mut OwnedReadHalf) -> Vec<u16>{
     buf
 }
 
-async fn add_dispatchers(dispatchers: &Dispatchers, roads: Vec<Road>, writer: &ConnectionHandler){
+async fn add_dispatchers(dispatchers: &Dispatchers, roads: Vec<Road>, writer: &ConnectionHandler) {
     let mut dispatchers = dispatchers.lock().await;
     for road in roads {
         let writer = writer.clone();
@@ -274,7 +310,7 @@ async fn add_dispatchers(dispatchers: &Dispatchers, roads: Vec<Road>, writer: &C
 async fn setup_dispatcher_thread(ticket_queue: TicketQueue, dispatchers: Dispatchers) {
     tokio::spawn(async move {
         loop {
-            sleep(Duration::from_millis(1000));
+            sleep(Duration::from_millis(1000)).await;
             {
                 let mut ticket_queue = ticket_queue.lock().await;
                 for _ in 0..ticket_queue.size() {
@@ -310,25 +346,27 @@ async fn setup_dispatcher_thread(ticket_queue: TicketQueue, dispatchers: Dispatc
     });
 }
 
-async fn send_ticket(ticket_queue: &TicketQueue, plate: &Plate, camera: &Camera, sighting: LimitBroke){
-    let ticket = Ticket{
+async fn send_ticket(ticket_queue: &TicketQueue, plate: &Plate, camera: &Camera, sighting: LimitBroke) {
+    let ticket = Ticket {
         plate: plate.plate.to_string(),
         road: camera.road,
         mile1: sighting.sighting1.mile,
         timestamp1: sighting.sighting1.timestamp,
         mile2: sighting.sighting2.mile,
         timestamp2: sighting.sighting2.timestamp,
-        speed: sighting.speed
+        speed: sighting.speed,
     };
     let mut ticket_queue = ticket_queue.lock().await;
     ticket_queue.add(ticket).unwrap();
 }
 
-async fn write_error(writer: &ConnectionHandler, message: String){
+async fn write_error(writer: &ConnectionHandler, message: String) {
+    eprintln!("Error: {:?}", message);
     let mut writer = writer.lock().await;
     writer.write_u8(0x10).await.unwrap();
     writer.write_u8(message.len() as u8).await.unwrap();
     writer.write_all(message.as_bytes()).await.unwrap();
+    writer.flush().await.unwrap();
 }
 
 
