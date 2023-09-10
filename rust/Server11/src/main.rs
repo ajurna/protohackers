814180c5270fb62088fa8fab62370838cddd1652
Ajurna <ajurna@gmail.com>
2023-09-10 23:10:47 +0100

    problem 11 started

--- rust/Server11/src/main.rs
+++ rust/Server11/src/main.rs
@@ -3,6 +3,27 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 const AUTHORITY_SERVER: &str = "pestcontrol.protohackers.com:20547";
 
+#[derive(Debug, Clone)]
+struct Hello {
+    id: u8,
+    protocol: Vec<u8>,
+    version: u32,
+    checksum: u8
+}
+
+#[derive(Debug, Clone)]
+struct Error {
+    id: u8,
+    message: Vec<u8>,
+    checksum: u8
+}
+
+#[derive(Debug)]
+enum Message {
+    Hello(Hello),
+    Error(Error)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Start the TCP listener for incoming client connections
@@ -19,20 +40,68 @@ async fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0u8; 4096];
 
     // Read from the stream
-    let n = stream.read(&mut buffer).await.expect("failed to read data");
+    // let n = stream.read(&mut buffer).await.expect("failed to read data");
+
+    match stream.read_u8().await {
+        Ok(_) => {}
+        Err(_) => {}
+    }
 
     // Handle the incoming data based on message type
-    match buffer[0] {
-        0x50 => handle_hello(&buffer[1..n]),
-        0x58 => handle_site_visit(&buffer[1..n]).await,
+    let response = match stream.read_u8().await.unwrap() {
+        0x50 => handle_hello(&mut stream).await,
+        // 0x58 => handle_site_visit(&buffer[1..n]).await,
         _ => {
-            println!("Unsupported message type: {}", buffer[0]);
+            Message::Error(Error{
+                id: 51,
+                message: vec![0x62, 0x61, 0x64],
+                checksum: 0x78,
+            })
         }
-    }
+    };
+
 }
 
-fn handle_hello(data: &[u8]) {
-    println!("Received a Hello message with protocol version: {}", u32::from_be_bytes([data[11], data[12], data[13], data[14]]));
+fn verify_message(message: Message) {
+
+    let check = match message {
+        Message::Hello(message) => {
+            let check = [
+                    (message.protocol.len() as u32).to_be_bytes(),
+                    message.version.to_be_bytes(),
+                ]
+                .iter()
+                .flatten()
+                .chain(message.protocol.iter())
+                .chain(std::iter::once(&message.id))
+                .chain(std::iter::once(&message.checksum))
+                .fold(0u8, |acc, &byte| acc.wrapping_add(byte));
+            check
+        }
+        Message::Error(message) => {
+            let check = (message.message.len() as u32).to_be_bytes().iter()
+                .chain(message.message.iter())
+                .chain(std::iter::once(&message.id))
+                .chain(std::iter::once(&message.checksum))
+                .fold(0u8, |acc, &byte| acc.wrapping_add(byte));
+            check
+        }
+    };
+
+}
+
+async fn handle_hello(stream: &mut TcpStream) -> Message {
+    let id:u8 = 0x50;
+    let protocol_length = stream.read_u32().await.unwrap();
+    let mut protocol = vec![0u8; protocol_length as usize];
+    for _ in [..protocol_length]{
+        protocol.push(stream.read_u8().await.unwrap());
+    }
+    let version = stream.read_u32().await.unwrap();
+    let checksum = stream.read_u8().await.unwrap();
+    let hello = Hello{id, protocol, version, checksum};
+    Message::Hello(hello)
+
 }
 
 async fn handle_site_visit(data: &[u8]) {
